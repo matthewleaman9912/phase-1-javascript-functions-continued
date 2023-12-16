c5f8f41ea64d941af1a7761b1b0fbc1bb8bbdbf5
MatthewLeaman <leaman.30@osu.edu>
2023-12-16 17:39:36 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(phrase) {
+    if (phrase != null) {
+        let say = "This Saturday, I want to " + phrase + "!";
+        return say
+    }
+    else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+function mondayWork(name = "go to the office") {
+    return "This Monday, I will " + name + ".";
+}
+
+function wrapAdjective(mark = "*"){
+    const part1 = "You are ";
+    return function(special = "special") {
+        return part1 + mark + special + mark + "!";
+    }
+}
